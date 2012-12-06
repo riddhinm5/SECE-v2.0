bf081bddfbaf4fea4b261fcbd039a74e86264241
Riddhi Mehta <rnm2119@columbia.edu>
2012-12-06 13:05:03 -0500

    dashboard accordion implemented

---
+++ Web/js/dashboard.js
@@ -0,0 +1,10 @@
+$(function(){
+	var latlng = new google.maps.LatLng(40.809038567298586, -73.96126449108124);
+	var myOptions = {
+			zoom: 10,
+			center: latlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+
+	var map = new google.maps.Map(document.getElementById("map"), myOptions);
+});
